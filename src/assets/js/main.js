6a0459bd4c8eea08b0da2017140b5d655248d33f
Branden Higby <21283089+higby@users.noreply.github.com>
2021-04-13 23:00:51 -0700

    Custom easing for table of contents

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -131,14 +131,14 @@ window.addEventListener("popstate", (event) => {
 
 $(document).on("click", ".toc li span", function () {
   if ($(this).next().next().is(":hidden")) {
-    $(this).next().next().slideDown("fast");
+    $(this).next().next().slideDown("fast", "easeOutQuad");
     $(this)
       .children()
       .replaceWith(
         '<svg height="24" viewBox="0 -5 24 24" width="24" aria-hidden="true" fill="RGBA(0,0,0,.6)"><path fill-rule="evenodd" d="M18.78 15.28a.75.75 0 000-1.06l-6.25-6.25a.75.75 0 00-1.06 0l-6.25 6.25a.75.75 0 101.06 1.06L12 9.56l5.72 5.72a.75.75 0 001.06 0z"></path></svg>'
       );
   } else {
-    $(this).next().next().slideUp("fast");
+    $(this).next().next().slideUp("fast", "easeOutQuad");
     $(this)
       .children()
       .replaceWith(
