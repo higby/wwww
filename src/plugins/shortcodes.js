ce58ca9b8b5e6f3af1a3079b95d953607734fa34
Branden Higby <branden@higby.io>
2022-03-20 06:20:06 -0700

    New Opengraph Images

--- src/plugins/shortcodes.js
+++ src/plugins/shortcodes.js
@@ -1,5 +1,9 @@
 module.exports = function (config) {
 
+  config.addShortcode("OG", function (data) {
+    return encodeURIComponent(`https://higby.io/og${data}`);
+  });
+
   config.addPairedShortcode("encode", function (svg) {
     return encodeURIComponent(svg);
   });
