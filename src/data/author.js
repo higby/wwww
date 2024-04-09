6d83e106633be85550df170e5f9ec408504107e4
Branden Higby <branden@higby.io>
2024-04-09 01:12:51 -0700

    Update Mastodon Instance

--- src/data/author.js
+++ src/data/author.js
@@ -2,7 +2,7 @@ export default {
   name: 'Branden Higby',
   email: 'branden@higby.io',
   codeberg: 'https://codeberg.org/higby/',
-  mastodon: 'https://indieweb.social/@higby',
+  mastodon: 'https://front-end.social/@higby',
   youtube: 'https://www.youtube.com/@higby',
   letterboxd: 'https://letterboxd.com/higby_/'
 }
